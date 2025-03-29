965744e5444e4e11c97998297768ce64420c140f
Dov Lekhter <ivan.vendelovskyi123@gmail.com>
2025-03-29 12:44:03 +0300

    TwoSum1 solution

--- src/main/java/org/example/TwoSum1.java
+++ src/main/java/org/example/TwoSum1.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class TwoSum1 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numersMap = new HashMap<>();
+        //can skip array range chech since it is gurantied that answer exists
+        for (int i = 0; ; i++) {
+            int currentNum = nums[i];
+            int secondNum = target - currentNum;
+            if (numersMap.containsKey(secondNum)) {
+                return new int[] { i, numersMap.get(secondNum) };
+            }
+            numersMap.put(currentNum, i);
+        }
+    }
 }
